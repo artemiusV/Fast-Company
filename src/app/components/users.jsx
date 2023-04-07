0ef24e9e156da842089c2dfb108291704fb3e44f
artemiusV <artemius.visotski@gmail.com>
2023-04-07 12:15:56 +0300

    implemented sorting

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -8,12 +8,14 @@ import GroupList from "./groupList";
 import api from "../api";
 import SearchStatus from "./searchStatus";
 import UserTable from "./usersTable";
+import _ from "lodash";
 
 const Users = ({ users: allUsers, ...rest }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [professions, setProfession] = useState();
   const [selectedProf, setSelectedProf] = useState();
-  const pageSize = 4;
+  const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" });
+  const pageSize = 8;
 
   useEffect(() => {
     api.professions.fetchAll().then((data) => setProfession(data));
@@ -31,6 +33,17 @@ const Users = ({ users: allUsers, ...rest }) => {
     setSelectedProf(item);
   };
 
+  const handleSort = (item) => {
+    if (sortBy.iter === item) {
+      setSortBy((prevState) => ({
+        ...prevState,
+        order: prevState.order === "asc" ? "desc" : "asc",
+      }));
+    } else {
+      setSortBy({ iter: item, order: "asc" });
+    }
+  };
+
   const filteredUsers = selectedProf
     ? allUsers.filter(
         (user) =>
@@ -39,8 +52,9 @@ const Users = ({ users: allUsers, ...rest }) => {
     : allUsers;
 
   const count = filteredUsers.length;
+  const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order]);
 
-  const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+  const usersCrop = paginate(sortedUsers, currentPage, pageSize);
   const clearFilter = () => {
     setSelectedProf();
   };
@@ -63,7 +77,9 @@ const Users = ({ users: allUsers, ...rest }) => {
       )}
       <div className="d-flex flex-column">
         <SearchStatus length={count} />
-        {count > 0 && <UserTable users={usersCrop} {...rest} />}
+        {count > 0 && (
+          <UserTable users={usersCrop} onSort={handleSort} {...rest} />
+        )}
         <div className="d-flex justify-content-center">
           <Pagination
             itemsCount={count}
